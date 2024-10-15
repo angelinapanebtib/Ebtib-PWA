4d91ce9414449ccc6846db777dde67591f4ceff5
angelinapanebtib <angelina091766@gmail.com>
2024-10-15 10:31:03 -0700

    Create HomePage.js

---
+++ src/HomePage.js
@@ -0,0 +1,19 @@
+// src/pages/HomePage.js
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <h1 className="title">Ebtib</h1>
+      <p className="subtitle">Discover Personalized Care</p>
+      <div className="support-options">
+        <button className="support-btn">Receive support</button>
+        <button className="support-btn">Offer support</button>
+      </div>
+      <button className="next-btn">Next</button>
+    </div>
+  );
+};
+
+export default HomePage;
